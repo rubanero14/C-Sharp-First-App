9251e7136d25bf683da01248caa7962d6811e1cb
Rajaruban Rajindram <ahyat888@gmail.com>
2022-10-07 16:53:57 +0800

    logging data types

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 
-namespace HelloWorld
+namespace Variables
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var number = 2;
+            var count = 10;
+            var totalPrice = 20.95f;
+            var character = 'A';
+            var firstName = "Raj";
+            var isTrue = true;
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isTrue);
         }
     }
 }
