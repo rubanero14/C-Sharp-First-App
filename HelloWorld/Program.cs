43648d585226fee41528f6b5da0af4031b19c6a9
Rajaruban Rajindram <ahyat888@gmail.com>
2022-10-07 17:05:40 +0800

    size of integral and real numbers

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,18 +6,11 @@ namespace Variables
     {
         static void Main(string[] args)
         {
-            var number = 2;
-            var count = 10;
-            var totalPrice = 20.95f;
-            var character = 'A';
-            var firstName = "Raj";
-            var isTrue = true;
-            Console.WriteLine(number);
-            Console.WriteLine(count);
-            Console.WriteLine(totalPrice);
-            Console.WriteLine(character);
-            Console.WriteLine(firstName);
-            Console.WriteLine(isTrue);
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+            Console.WriteLine("{0} {1}", double.MinValue, double.MaxValue);
+            Console.WriteLine("{0} {1}", int.MinValue, int.MaxValue);
+            Console.WriteLine("{0} {1}", decimal.MinValue, decimal.MaxValue);
         }
     }
 }
