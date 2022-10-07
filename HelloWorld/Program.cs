2678e76044c8e9f59e7ea56ff6ecc27d57fa2266
Rajaruban Rajindram <ahyat888@gmail.com>
2022-10-07 17:08:24 +0800

    declaring const variable

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,11 +6,7 @@ namespace Variables
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
-            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
-            Console.WriteLine("{0} {1}", double.MinValue, double.MaxValue);
-            Console.WriteLine("{0} {1}", int.MinValue, int.MaxValue);
-            Console.WriteLine("{0} {1}", decimal.MinValue, decimal.MaxValue);
+            const float Pi = 3.14f;
         }
     }
 }
