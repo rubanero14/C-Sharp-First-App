9bd983d54bb28cfc3f3fa0c075358d19812f1844
Rajaruban Rajindram <ahyat888@gmail.com>
2022-11-23 10:37:53 +0800

    Classes Basics

---
+++ OOP-Creating-Classes/Human.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Creating_Classes
+{
+    // this class is a blueprint for a data type
+    class Human
+    {
+        // member variable, remove keyword public or change to private to avoid external classes changing its value
+        public string firstName;
+        public string lastName;
+        
+        public Human(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        // member
+        public void IntroMyself()
+        {
+            Console.WriteLine($"Hi, my name is {firstName} {lastName}");
+        }
+    }
+}
---
+++ OOP-Creating-Classes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Creating_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create object of my class, also called an instance of Human class
+            Human raj = new Human("Raj", "Ruban");
+            raj.IntroMyself();
+            Human sharm = new Human("Guga", "Sharm");
+            sharm.IntroMyself();
+
+            // Excercise
+
+            
+            Console.Read();
+        }
+    }
+}
