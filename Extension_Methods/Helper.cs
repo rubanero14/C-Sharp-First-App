df1bd4cc60d3202e07cec8c074108beabeaaa227
Rajaruban Rajindram <ahyat888@gmail.com>
2022-12-16 21:36:21 +0800

    Extension Methods - Building Custom Helper Extension Methods (Abstraction)

---
+++ Extension_Methods/Helper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension_Methods
+{
+    // Adding static keyword to the class Helper enables it to become custom extension method, must be paid with 'this' keyword in the nested method to enable this extension
+    // method can be accessed without passing any arguements. Can be accessed by appending it to any string
+    public static class Helper
+    {
+        // Adding this keyword into the method args before type enables this method can be accessesd without args, only applicable for first parameter
+        public static string ChangeFirstCharToLowerOrUpper(this string inputStr)
+        {
+            if (inputStr.Length > 0)
+            {
+                char[] charArray = inputStr.ToCharArray();
+                charArray[0] = char.IsUpper(charArray[0]) ? char.ToLower(charArray[0]) : char.ToUpper(charArray[0]);
+                var result = new string(charArray);
+                return result;
+            }
+
+            return inputStr;
+        }
+
+        public static List<int> EvenNumbers(this List<int> inputInts)
+        {
+            if (inputInts.Count > 0)
+            {
+                List<int> result = inputInts.Where(x => x % 2 == 0).ToList();
+                return result;
+            }
+
+            return inputInts;
+        }
+    }
+}
---
+++ Extension_Methods/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Method for String type
+            string name = "rajaruban";
+
+            var formattedNameOne = Helper.ChangeFirstCharToLowerOrUpper(name); // Helper method without static keyword, passing string as args
+            Console.WriteLine(formattedNameOne);
+
+            var formattedNameTwo = name.ChangeFirstCharToLowerOrUpper(); // Helper method becomes an Extension Method just by appending to the string, not by psing as args
+            Console.WriteLine(formattedNameTwo);
+
+            // MethodAccessException for List<int> type
+            List<int> Numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; // List of Numbers
+
+            // Using built-in Enumerable extension class method with LINQ Lambda Expression by passing 2 args, first param is List of numbers, second is Lambda Expression
+            IEnumerable<int> evenNumbersOne = Enumerable.Where(Numbers, x => x % 2 == 0);
+            foreach (int number in evenNumbersOne)
+            {
+                Console.WriteLine(number);
+            }
+
+            List<int> evenNumbersTwo = Helper.EvenNumbers(Numbers); // Helper method without static keyword, passing List<int> as args
+            
+            // Since this is ait, we have to use foreach method to print to console
+            foreach(var num in evenNumbersTwo)
+            {
+                Console.WriteLine(num);
+            }
+
+            // Since this is ait, we have to use foreach method to print to console
+            List<int> evenNumberThree = Numbers.EvenNumbers();  // Helper method becomes an Extension Method just by appending to the List<int>, not by psing as args
+            foreach (var num in evenNumberThree)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
