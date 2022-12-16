6d0fd1d92fc0b32c2fd6bca61e57ea457636bc69
Rajaruban Rajindram <ahyat888@gmail.com>
2022-12-16 16:43:22 +0800

    LINQ using Lambda Expressions

---
+++ LINQ Lambda Expressions/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Lambda_Expressions
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(int id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+        }
+
+        public static List<Person> GetAllPeople()
+        {
+            List<Person> people = new List<Person>();
+
+            Person person_one = new Person(1, "Raj", 35);
+            people.Add(person_one);
+
+            Person person_two = new Person(2, "Sham", 34);
+            people.Add(person_two);
+
+            Person person_three = new Person(3, "Axa", 4);
+            people.Add(person_three);
+
+            return people;
+
+        }
+    }
+}
---
+++ LINQ Lambda Expressions/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Lambda_Expressions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Person> filteredPerson = Person.GetAllPeople()/*.Where(x => x.Id == 1)*/;
+
+            Console.WriteLine("List of Person Registered:");
+
+            foreach(var person in filteredPerson)
+            {  
+                Console.WriteLine($"ID: {person.Id}, Name: {person.Name}, Age: {person.Age}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
