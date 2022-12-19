40826807f9728e873d1408e3b479375ecf2e45ab
Rajaruban Rajindram <ahyat888@gmail.com>
2022-12-19 22:55:30 +0800

    Aggregate_Function_in_LINQ - Added Examples with and without LINQ Aggregate Functions

--- Aggregate_Function_in_LINQ/Program.cs
+++ Aggregate_Function_in_LINQ/Program.cs
@@ -11,14 +11,31 @@ namespace Aggregate_Function_in_LINQ
     {
         static void Main(string[] args)
         {
+            // Instiating string Array of Countries
             string[] Countries = { "Malaysia", "Singapore", "Indonesia", "Thailand", "India", "Australia", "New Zealand", "USA" };
 
+            // Instiating int Array of Numbers
+            int[] Numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
             // Without LINQ Aggregate Function, using custom extension method class
-            string stringOfCountriesNoLINQ = Countries.Stringify();
-            Console.WriteLine($"Stringified List from Array without LINQ: {stringOfCountriesNoLINQ}\n");
+            string stringOfCountriesNoLINQ = Countries.StringifyStringArray();
+            int productOfAllNumbersNoLINQ = Numbers.ProductOfAllNumbers();
+
+            Console.WriteLine($"Stringified string Array of Countries without LINQ:\n{stringOfCountriesNoLINQ}\n");
+            Console.WriteLine($"Product of all Numbers in int Array without LINQ:\n{productOfAllNumbersNoLINQ}\n");
 
             // With LINQ Aggregate Function
+            string stringOfCountries = Countries.Aggregate((a, b) => $"{a}, {b}");
 
+            // Equivalent to .reduce() function in Javascript
+            int productOfAllNumbers = Numbers.Aggregate((a, b) => a * b);
+
+            // Seed parameter is a initial assigned value to variable a where Aggregate funtion will take as first param and second param is Lambda expression, where seed value will be the first variable
+            int productOfAllNumbersWithSeedParam = Numbers.Aggregate(2, (a, b) => a * b); 
+
+            Console.WriteLine($"Stringified string Array of Countries with LINQ Aggregate Function:\n{stringOfCountries}\n");
+            Console.WriteLine($"Product of all Numbers in int Array with LINQ Aggregate Function:\n{productOfAllNumbers}\n");
+            Console.WriteLine($"Product of all Numbers in int Array with LINQ Aggregate Function with Seed Parameter (Initial Value):\n{productOfAllNumbersWithSeedParam}\n");
 
             Console.ReadLine();
         }
--- Aggregate_Function_in_LINQ/WithoutLINQ.cs
+++ Aggregate_Function_in_LINQ/WithoutLINQ.cs
@@ -8,13 +8,25 @@ namespace Aggregate_Function_in_LINQ
 {
     public static class WithoutLINQ
     {
-        public static string Stringify(this string[] listItem)
+        public static string StringifyStringArray(this string[] listItem)
         {
             string result = string.Empty;
 
             foreach (var item in listItem)
             {
                 result = result + item + ", ";
+            }
+            result = result.EndsWith(", ") ? result.Remove(result.Length - 2) : result;
+            return result;
+        }
+
+        public static int ProductOfAllNumbers(this int[] listItem)
+        {
+            int result = 1;
+
+            foreach (var item in listItem)
+            {
+                result *= item;
             }
             return result;
         }
